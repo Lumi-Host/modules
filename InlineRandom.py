8bfe991b90558d68c852507090f3e0393742741d
VadymYem <81570039+VadymYem@users.noreply.github.com>
2023-01-13 16:51:24 +0200

    Update InlineRandom.py

--- InlineRandom.py
+++ InlineRandom.py
@@ -1,5 +1,3 @@
-
-
 from random import choice, randint
 
 from .. import loader, utils
@@ -8,7 +6,7 @@ from ..inline.types import InlineQuery
 
 @loader.tds
 class InlineRandomMod(loader.Module):
-    """Random tools for your userbot"""
+    """Random tools for your bot"""
 
     strings = {"name": "InlineRandom"}
 
@@ -16,18 +14,18 @@ class InlineRandomMod(loader.Module):
     async def coin_inline_handler(self, query: InlineQuery) -> dict:
         """Heads or tails?"""
 
-        r = "🦅 Heads" if randint(0, 1) else "🪙 Tails"
+        r = "🦅 Орел" if randint(0, 1) else "🪙 Решка"
 
         return {
-            "title": "Toss a coin",
-            "description": "Trust in the God of luck, and he will be by your side!",
-            "message": f"<i>The God of luck tells us...</i> <b>{r}</b>",
+            "title": "Кинути монетку ",
+            "description": "Довіртеся Богу удачі, і він буде поруч з вами!",
+            "message": f"<i>Бог удачі шепоче нам...</i> <b>{r}</b>",
             "thumb": "https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-coin-pirates-justicon-flat-justicon-1.png",
         }
 
     @loader.inline_everyone
     async def random_inline_handler(self, query: InlineQuery) -> dict:
-        """[number] - Send random number less than specified"""
+        """[Число] - Надіслати випадкове число менше вказаного"""
 
         if not query.args:
             return
@@ -38,15 +36,15 @@ class InlineRandomMod(loader.Module):
             return
 
         return {
-            "title": f"Toss random number less or equal to {a}",
-            "description": "Trust in the God of luck, and he will be by your side!",
-            "message": f"<i>The God of luck screams...</i> <b>{randint(1, int(a))}</b>",
+            "title": f"Випадкове число менше або дорівнює {a}",
+            "description": "Довіртеся Богу удачі, і він буде поруч з вами!",
+            "message": f"<i>Бог удачі шепоче...</i> <b>{randint(1, int(a))}</b>",
             "thumb": "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-numbers-auction-house-flaticons-flat-flat-icons.png",
         }
 
     @loader.inline_everyone
     async def choice_inline_handler(self, query: InlineQuery) -> dict:
-        """[args, separated by comma] - Make a choice"""
+        """[аргументи, розділені комою] - Зробити вибір"""
 
         if not query.args or not query.args.count(","):
             return
@@ -54,20 +52,11 @@ class InlineRandomMod(loader.Module):
         a = query.args
 
         return {
-            "title": "Choose one item from list",
-            "description": "Trust in the God of luck, and he will be by your side!",
+            "title": "Виберіть один пункт зі списку",
+            "description": "Довіртеся Богу удачі, і він буде поруч з вами!",
             "message": (
                 "<i>Бог удачі шепоче...</i>"
                 f" <b>{choice(a.split(',')).strip()}</b>"
             ),
             "thumb": "https://img.icons8.com/external-filled-outline-geotatah/64/000000/external-choice-customer-satisfaction-filled-outline-filled-outline-geotatah.png",
-        }
-
-    @loader.inline_everyone
-    async def person_inline_handler(self, query: InlineQuery) -> dict:
-        """This person doesn't exist"""
-
-        return {
-            "photo": f"https://thispersondoesnotexist.com/image?id={utils.rand(10)}",
-            "title": "This person doesn't exist",
         }
